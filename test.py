a0359aa14be8cb130c35dd9e798ef118b2b74757
Shwedovski <danya.makovey1@gmail.com>
2025-01-30 00:39:10 +0200

    тест

---
+++ test.py
@@ -0,0 +1,31 @@
+from telegram import Update, Poll
+from telegram.ext import Application, CommandHandler, CallbackContext
+
+TOKEN = "7908032837:AAHcXz8_yPBILErqpBjCi2EE2aUyNI87Xqc"
+
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text("Напиши /game чтобы создать опрос")
+
+async def game(update: Update, context: CallbackContext):
+    question = "Когда играть? "
+    options = ["18:00", "18:30", "19:00", "19:30", "никаких игров"]
+
+    await context.bot.send_poll(
+        chat_id=update.message.chat_id,
+        question=question,
+        options=options,
+        is_anonymous=False, 
+        allows_multiple_answers=True,
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("game", game))
+
+    print("Бот запущен...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
